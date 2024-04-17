eb726c73849139286df97879b9ff6e4f45eccbf7
johana <v95jgl@gmail.com>
2024-04-17 17:32:01 -0600

    Feat: add pokemon component

---
+++ src/app/components/pokemon/pokemon.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-pokemon',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './pokemon.component.html',
+})
+export class PokemonComponent {}
