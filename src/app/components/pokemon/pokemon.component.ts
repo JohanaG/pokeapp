7fc5265d5985a0527dd2a41ba4ef0cb37ebe9603
johana <v95jgl@gmail.com>
2024-04-17 21:45:22 -0600

    Feat: update pokemon and pokedex component

--- src/app/components/pokemon/pokemon.component.ts
+++ src/app/components/pokemon/pokemon.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IPokemonModel } from 'src/domain/models/pokemon.model';
+import { ChipsComponent } from '../chips/chips.component';
 
 @Component({
   selector: 'app-pokemon',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ChipsComponent],
   templateUrl: './pokemon.component.html',
 })
-export class PokemonComponent {}
+export class PokemonComponent {
+  @Input({ required: true })
+  pokemon?: IPokemonModel;
+}
--- src/app/pages/pokedex/pokedex/pokedex.component.ts
+++ src/app/pages/pokedex/pokedex/pokedex.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PokemonComponent } from 'src/app/components/pokemon/pokemon.component';
+import { DataModule } from 'src/data/data.module';
+import { GetPokemonAllUseCase } from 'src/usecases/pokemon/get-pokemon-all.usecase';
 
 @Component({
   selector: 'app-pokedex',
   standalone: true,
-  imports: [CommonModule, PokemonComponent],
+  imports: [CommonModule, PokemonComponent, DataModule],
   templateUrl: './pokedex.component.html',
 })
-export class PokedexComponent {}
+export class PokedexComponent {
+  getPokemonAll = inject(GetPokemonAllUseCase);
+  getPokemons = this.getPokemonAll.execute();
+}
