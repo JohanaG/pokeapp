6d536abeb011372afbb8819a863c94ed71a54422
johana <v95jgl@gmail.com>
2024-04-18 14:35:44 -0600

    Feat: create component pokemon detail

---
+++ src/app/pages/pokedex/pokemon-detail/pokemon-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-pokemon-detail',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './pokemon-detail.component.html'
+})
+export class PokemonDetailComponent {
+
+}
