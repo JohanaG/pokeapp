18d56932af29c92af993ce9f3baa4b5ac9b847ab
johana <v95jgl@gmail.com>
2024-04-18 17:10:59 -0600

    Feat: new route

--- src/app/pages/pokedex/routes.ts
+++ src/app/pages/pokedex/routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { PokedexComponent } from './pokedex/pokedex.component';
+import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
 
-export const routes: Routes = [{ path: '', component: PokedexComponent }];
+export const routes: Routes = [
+  { path: '', component: PokedexComponent },
+  { path: ':id', component: PokemonDetailComponent },
+];
