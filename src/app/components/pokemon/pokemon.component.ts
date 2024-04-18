3770019e3a7babdcb1970e06083e1f64eca70d96
johana <v95jgl@gmail.com>
2024-04-18 10:46:16 -0600

    Feat: show image  and type pokemon

--- src/app/components/pokemon/pokemon.component.ts
+++ src/app/components/pokemon/pokemon.component.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { IPokemonModel } from 'src/domain/models/pokemon.model';
 import { ChipsComponent } from '../chips/chips.component';
 import { GetPokemonByIdUseCase } from 'src/usecases/pokemon/get-pokemon-by-id.usecase';
+import { BehaviorSubject, Subject, switchMap, tap } from 'rxjs';
+import { GetPokemonID } from 'src/app/utils/helpers/get-pokemon-id';
 
 @Component({
   selector: 'app-pokemon',
@@ -12,7 +14,16 @@ import { GetPokemonByIdUseCase } from 'src/usecases/pokemon/get-pokemon-by-id.us
 })
 export class PokemonComponent {
   private pokemonById = inject(GetPokemonByIdUseCase);
-  @Input({ required: true })
-  pokemon?: IPokemonModel;
-  getPokemon = this.pokemonById.execute(1);
+  setPokemonId = new BehaviorSubject<number>(0);
+  _pokemon?: IPokemonModel;
+  pokemonId?: number;
+  getPokemon = this.setPokemonId.pipe(
+    switchMap((value) => this.pokemonById.execute(value)),
+  );
+
+  @Input({ required: true }) set pokemon(pokemon: IPokemonModel) {
+    this._pokemon = pokemon;
+    this.pokemonId = Number(GetPokemonID(pokemon.url));
+    this.setPokemonId.next(this.pokemonId);
+  }
 }
