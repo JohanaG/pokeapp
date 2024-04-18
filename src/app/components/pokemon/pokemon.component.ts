f03328d661b681528b3501c299c7cf933fc77097
johana <v95jgl@gmail.com>
2024-04-17 23:01:12 -0600

    Feat: add pokemon by id use case to pokemon component

--- src/app/components/pokemon/pokemon.component.ts
+++ src/app/components/pokemon/pokemon.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IPokemonModel } from 'src/domain/models/pokemon.model';
 import { ChipsComponent } from '../chips/chips.component';
+import { GetPokemonByIdUseCase } from 'src/usecases/pokemon/get-pokemon-by-id.usecase';
 
 @Component({
   selector: 'app-pokemon',
@@ -10,6 +11,8 @@ import { ChipsComponent } from '../chips/chips.component';
   templateUrl: './pokemon.component.html',
 })
 export class PokemonComponent {
+  private pokemonById = inject(GetPokemonByIdUseCase);
   @Input({ required: true })
   pokemon?: IPokemonModel;
+  getPokemon = this.pokemonById.execute(1);
 }
