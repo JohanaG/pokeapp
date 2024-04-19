f87328470be604633f06edd26405229729455d25
johana <v95jgl@gmail.com>
2024-04-18 22:38:26 -0600

    feat: update description and category field

--- src/app/pages/pokedex/pokemon-detail/pokemon-detail.component.ts
+++ src/app/pages/pokedex/pokemon-detail/pokemon-detail.component.ts
@@ -1,12 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ActivatedRoute, Params, RouterLink } from '@angular/router';
-import { map, switchMap, tap } from 'rxjs';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { switchMap, tap } from 'rxjs';
 import { GetPokemonByIdUseCase } from 'src/usecases/pokemon/get-pokemon-by-id.usecase';
 import { DataModule } from 'src/data/data.module';
 import { ChipsComponent } from 'src/app/components/chips/chips.component';
 import { GetPokemonTypeByIdUseCase } from 'src/usecases/pokemon/get-pokemon-type-by-id.usecase';
 import { GetPokemonID } from 'src/app/utils/helpers/get-pokemon-id';
+import { GetPokemonSpeciesByIdUseCase } from 'src/usecases/pokemon/get-pokemon-species.usecase';
 
 @Component({
   selector: 'app-pokemon-detail',
@@ -17,22 +18,34 @@ import { GetPokemonID } from 'src/app/utils/helpers/get-pokemon-id';
 export class PokemonDetailComponent {
   route = inject(ActivatedRoute);
   private getPokemonById = inject(GetPokemonByIdUseCase);
-  private getPokemonTypeByID = inject(GetPokemonTypeByIdUseCase);
+  private getPokemonTypeById = inject(GetPokemonTypeByIdUseCase);
+  private getPokemonSpeciesId = inject(GetPokemonSpeciesByIdUseCase);
+  pokemonId = 0;
 
   pokemon = this.route.params.pipe(
     switchMap((value) => {
-      const pokemonId = Number(value['id']);
-      return this.getPokemonById.execute(pokemonId);
+      this.pokemonId = Number(value['id']);
+      return this.getPokemonById.execute(this.pokemonId);
     }),
     tap((value) => console.log(value)),
   );
 
   pokemonWeaknesses = this.pokemon.pipe(
     switchMap((pokemon) => {
-      const pokemonTypeUrl = pokemon.types[0].url;
-      const pokemonTypId = Number(GetPokemonID(pokemonTypeUrl));
-      return this.getPokemonTypeByID.execute(pokemonTypId);
+      const pokemonTypeId = this.getId(pokemon.types[0].url);
+      return this.getPokemonTypeById.execute(pokemonTypeId);
     }),
     tap((value) => console.log(value)),
   );
+
+  pokemonSpecies = this.pokemon.pipe(
+    switchMap((pokemon) => {
+      const pokemSpeciesId = this.getId(pokemon.species.url);
+      return this.getPokemonSpeciesId.execute(pokemSpeciesId);
+    }),
+  );
+
+  getId(url: string): number {
+    return Number(GetPokemonID(url));
+  }
 }
