8e4f06e35573d728c4844b2d8aaa6b2a6ee9edc9
johana <v95jgl@gmail.com>
2024-04-19 16:15:27 -0600

    Feat: create new route habilidades

--- src/app/pages/ability/routes.ts
+++ src/app/pages/ability/routes.ts
@@ -1,4 +1,7 @@
 import { Routes } from '@angular/router';
 import { AbilityComponent } from './ability/ability.component';
 
-export const routes: Routes = [{ path: '', component: AbilityComponent }];
+export const routes: Routes = [
+  { path: '', component: AbilityComponent },
+  { path: ':name', component: AbilityComponent },
+];
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { PokedexComponent } from './pages/pokedex/pokedex/pokedex.component';
 import { DefaultLayoutComponent } from './layout/default-layout/default-layout.component';
 
 export const routes: Routes = [
