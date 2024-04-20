0cc7c2401f12bad9a7a4cf2f5d748a9b2fd1a902
johana <v95jgl@gmail.com>
2024-04-19 18:26:59 -0600

    Feat: add image default in pokemon detail mapper

--- src/data/repositories/pokemon-repository/mapper/pokemon-detail.mapper.ts
+++ src/data/repositories/pokemon-repository/mapper/pokemon-detail.mapper.ts
@@ -2,16 +2,16 @@ import { Mapper } from 'src/base/mapper';
 import { IPokemonDetailsEntity } from '../entity/pokemon-details.entity';
 import { IPokemonDetailModel } from 'src/domain/models/pokemon-details.model';
 import { PokemonTypes } from 'src/app/models/enums/pokemon-type.enum';
-import { GetPokemonID } from 'src/app/utils/helpers/get-pokemon-id';
 
 export class PokemonDetailMapper extends Mapper<
   IPokemonDetailsEntity,
   IPokemonDetailModel
 > {
   mapFrom(param: IPokemonDetailsEntity): IPokemonDetailModel {
+    const imageDefault = param.sprites.front_default;
     return {
       id: param.id,
-      cover: param.sprites.other?.dream_world.front_default,
+      cover: param.sprites.other?.dream_world.front_default ?? imageDefault,
       types: param.types.map((value) => {
         return { ...value.type, background: PokemonTypes[value.type.name] };
       }),
