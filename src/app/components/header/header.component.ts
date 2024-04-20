3eb8054788ad0704166d4b41381650b98be04c40
johana <v95jgl@gmail.com>
2024-04-20 00:48:02 -0600

    Feat: modify design header and layout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,24 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IMenuHeader } from 'src/app/models/interfaces/menu-header.interface';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {
-  menu: IMenuHeader[] = [
-    {
-      title: 'Home',
-      class: 'hover:bg-red-600 border-b-red-600',
-      url: '',
-    },
-    {
-      title: 'Pokedex',
-      class: 'hover:bg-green-600 border-b-green-600',
-      url: '',
-    },
-  ];
-}
+export class HeaderComponent {}
