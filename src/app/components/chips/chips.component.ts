c1d88d55716bace3951d00d6fe3a9eb0c39287c1
johana <v95jgl@gmail.com>
2024-04-19 19:30:19 -0600

    Feat: add label a on chips option

--- src/app/components/chips/chips.component.ts
+++ src/app/components/chips/chips.component.ts
@@ -1,13 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IPokemonTypeModel } from 'src/domain/models/pokemon-type.model';
+import { RouterLink } from '@angular/router';
+import { GetPokemonID } from 'src/app/utils/helpers/get-pokemon-id';
 
 @Component({
   selector: 'app-chips',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './chips.component.html',
 })
 export class ChipsComponent {
   @Input() chips: IPokemonTypeModel[] = [];
+
+  getId(url: string): number {
+    return Number(GetPokemonID(url));
+  }
 }
