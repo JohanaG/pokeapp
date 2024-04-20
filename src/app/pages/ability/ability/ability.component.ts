2a15c9424a710ae48731dc7115983427acbcdf87
johana <v95jgl@gmail.com>
2024-04-19 18:28:01 -0600

    Feat: design and funcionality in ability component

--- src/app/pages/ability/ability/ability.component.ts
+++ src/app/pages/ability/ability/ability.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+
+import { switchMap } from 'rxjs';
+import { GetPokemonAbilityUseCase } from 'src/usecases/pokemon/get-pokemon-ability-by-name.usecase';
+import { DataModule } from 'src/data/data.module';
+import { PokemonComponent } from 'src/app/components/pokemon/pokemon.component';
+import { HeaderPokemonComponent } from 'src/app/components/header-pokemon/header-pokemon.component';
 
 @Component({
   selector: 'app-ability',
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './ability.component.html'
+  imports: [CommonModule, DataModule, PokemonComponent, HeaderPokemonComponent],
+  templateUrl: './ability.component.html',
 })
 export class AbilityComponent {
+  route = inject(ActivatedRoute);
+  private getPokemonAbility = inject(GetPokemonAbilityUseCase);
 
+  ability = this.route.params.pipe(
+    switchMap((value) => {
+      return this.getPokemonAbility.execute(value['name']);
+    }),
+  );
 }
