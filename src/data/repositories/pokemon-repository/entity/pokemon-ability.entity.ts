8bb7fd5901b50a04f5b71fddd827bed91bba0fa6
johana <v95jgl@gmail.com>
2024-04-19 19:31:05 -0600

    Feat: update ability entirty

--- src/data/repositories/pokemon-repository/entity/pokemon-ability.entity.ts
+++ src/data/repositories/pokemon-repository/entity/pokemon-ability.entity.ts
@@ -1,26 +1,23 @@
+import { IPokemonModel } from 'src/domain/models/pokemon.model';
+
 export interface IAbilityEntity {
   flavor_text_entries: IFlavorTextEntry[];
   names: IName[];
   pokemon: IPokemon[];
 }
 
-export interface IGeneration {
-  name: string;
-  url: string;
-}
-
 export interface IFlavorTextEntry {
   flavor_text: string;
-  language: IGeneration;
+  language: IPokemonModel;
 }
 
 export interface IName {
-  language: IGeneration;
+  language: IPokemonModel;
   name: string;
 }
 
 export interface IPokemon {
   is_hidden: boolean;
-  pokemon: IGeneration;
+  pokemon: IPokemonModel;
   slot: number;
 }
